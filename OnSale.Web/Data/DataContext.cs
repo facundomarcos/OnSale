6508e5e7a2438cf7c49cfd46bf186a07f2d267e4
facundomarcos <facundomarcos@live.com.ar>
2020-11-06 21:20:35 -0300

    improve countries CRUD

---
+++ OnSale.Web/Data/DataContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OnSale.Common.Entities;
+
+namespace OnSale.Web.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+        //mapeo a la bd
+        public DbSet<Country> Countries { get; set; }
+        //este metodo crea un indice para que no haya 2 paises con el mismo nombre
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Country>()
+                .HasIndex(t => t.Name)
+                .IsUnique();
+        }
+    }
+
+}
